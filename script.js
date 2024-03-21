1c567b53ad02bee12393f6650bade83038495e7b
Stefanita Matei <stefanita.matei@emanuel.ro>
2024-03-21 12:44:07 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function setup(){
-createCanvas(800, 600);
-}
-function draw(){
+    createCanvas(800, 600);
+    }
     
-}
+    function draw(){
+        background ("yellow");
+    smilyFaceStefan();}
+    function smilyFaceStefan(){
+        //BODY
+        fill("white");
+        stroke("black")
+       circle (400, 300, 50)
+    
+       //EYES
+       stroke("brown");
+       ellipse(390, 290, 10, 5)
+       ellipse(410, 290, 10, 5)
+       //EYE PUPILS
+        stroke("black");
+       fill("black");
+       circle(390, 290, 2)
+       circle(410, 290,2)
+    
+       //MOUTH
+       stroke("green")
+       fill("black")
+       arc(400, 310, 10, 10, 0, Math.PI )
+    
+       //CHEEKS
+       stroke("pink");
+       fill("pink");
+       circle(420, 305, 7);
+       circle(380, 305, 7);
+    }
