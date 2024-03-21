ef299756edffa578326e2e3cc7e09dbbd85ab2e6
Mihai Vlad Eris <vlad.mihai@emanuel.ro>
2024-03-21 12:54:43 +0200

    smiley face

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ function setup(){
 createCanvas(800, 600);
 }
 function draw(){
+    background("green");
+    smileyFaceVlad();
+}
+ 
+function smileyFaceVlad() {
+    circle(400, 300, 50);
+
+    stroke("black")
+    circle(400,300,50);
+
+    stroke("brown");
+    ellipse(390,290,10,5);
+    ellipse(410,290,10,5);
+    circle(390,290,2);
+    circle(410,290,2);
     
 }
