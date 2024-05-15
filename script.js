ac7510f10e1c51820bfc4065b32f7503f1d27e77
stefan <146415173+1stefanita@users.noreply.github.com>
2024-05-16 01:08:14 +0300

    Commit patrat

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ function pion2(x, y) {
     fill('Red');
     ellipse(x, y, 30, 30);
 }
+class Patrat {
+    // constructor
+    initializare(patrat = null) {
+        if (patrat) {
+            // daca am primit un patrat
+            this.patrat = patrat; //copiaza patratul
+        } else {
+            // daca nu am primit un patrat
+            this.patrat = []; //initializeaza patratul
+            for (let r = 0; r < length; r++) {
+                //parcurgem patratul
+                this.patrat[r] = []; //initializeaza linia
+                for (let c = 0; c < length; c++) {
+                    //parcurgem linia
+                    let valueObj = {
+                        //initializeaza valoarea
+                        value: "0", //initializeaza valoarea
+                        color: "white", //initializeaza culoarea
+                    };
+                    this.patrat[r][c] = valueObj; //adauga valoarea in linia
+                }
+            }
+        }
+    }
